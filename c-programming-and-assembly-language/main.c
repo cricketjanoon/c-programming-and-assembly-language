969d33aa8084af448934ba880c3bf10b3de14359
Shahbaz Ali <shahbazlization@gmail.com>
2021-09-23 18:29:43 +0500

    lecture 07: optimized/un-optimized compiler output

--- c-programming-and-assembly-language/main.c
+++ c-programming-and-assembly-language/main.c
@@ -3,10 +3,53 @@
 
 void add4();
 void func1(); // this function contains examples form lecture 06
+void func2(); // this function contains examples from lecture 07
 
 int main()
 {
-    func1();
+    func2();
+}
+
+void func2() {
+
+    // compute: z = x/y, r = x % y
+    int x = 50;     //mov x, 0x000A
+    int y = 12;     //mov y, 0x0014
+    int z, r;
+    
+
+    // optimizing here, we perform one ALU operation and get the both results
+    // compiler would have done the ALU operation twice
+    __asm {
+            xor edx, edx
+            xor eax, eax
+            mov eax, x
+            div y
+            mov z, eax
+            mov r, edx
+    }
+
+    printf("qoutient: %d, rem: %d\n", z, r);
+
+    int a, b, c, d;
+    // a=x+y, b=a-y, c=b*y, d=c/y
+    // the compiler blindly translates these instrucitons and there are lots of redundant mov instrucions
+    
+    __asm {
+            mov eax, x
+            add eax, y
+            mov a, eax
+            sub eax, y
+            mov b, eax
+            mul y
+            mov c, eax
+            div y
+            mov d, eax
+    }
+
+    printf("a: %d, b: %d, c: %d, d: %d\n", a, b, c, d);
+
+
 }
 
 void func1() {
